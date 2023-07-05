cd3e0de6702b3f6f8717719237a3cc8a3e492684
Anh Duy <nguyenvoanhduy1@gmail.com>
2023-07-05 14:06:23 +0700

    first commit

---
+++ 1.py
@@ -0,0 +1,17 @@
+
+#eval la so
+#khong eval la string
+#temp = eval(input("Nhap 1 so: "))
+#print(type(temp))
+"""
+
+"""
+#sep
+#D = "Dep Trai"
+#print("Duy(",D,")",sep="+")
+#end
+#print("Dong 1",end="\t")
+#print("Dong 2")
+#   +,-,*,/
+a=45
+print(a//10)
